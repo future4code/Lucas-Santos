fb80b005853751ad02bcb3a413723648546dde32
lucasgomesoficial <lucasgomesbr@gmail.com>
2021-05-03 17:01:25 -0300

    Adicionando semana 1 da aula 3

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,53 @@
+// Exercícios de interpretação de código
+
+// 1) 10 e depois 10 e 5
+// 2) 10 10 10
+// 3) horaTrabalho e salarioDiario
+
+
+// Exercícios de escrita de código
+
+// 1) a. 
+let nome
+
+// b.
+let idade
+
+// c.
+console.log(typeof nome)
+console.log(typeof idade)
+
+// d. as variaveis são undefined pois não foram atribuidas um valor
+
+// e.
+nome = prompt('Qual é seu nome?')
+idade = prompt('Qual é a sua idade')
+
+// f.
+console.log(typeof nome)  // foi atribuido um valor
+console.log(typeof idade) // foi atribuido um valor
+
+// g.
+console.log('Olá', nome, 'você tem', idade, 'anos.')
+
+// 2)
+const firstQuestion = "Você está bem?"
+const secondQuestion = "Você tem e-mail?"
+const thirdQuestion = "Você Respira?"
+
+// a.
+const life = prompt(firstQuestion)
+const email = prompt(secondQuestion)
+const breathe = prompt(thirdQuestion)
+
+// b.
+console.log(firstQuestion, life)
+console.log(secondQuestion, email)
+console.log(thirdQuestion, breathe)
+
+// 3)
+let a = 10
+let b = 25
+
+a = 10
+b = 15
