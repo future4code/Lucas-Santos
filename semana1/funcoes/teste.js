40f44fddce8ede35c2b03c448d78b2a13aef80cc
lucasgomesoficial <lucasgomesbr@gmail.com>
2021-05-06 17:03:16 -0300

    excluido arquivo

--- semana1/funcoes/teste.js
+++
@@ -1,9 +0,0 @@
-const teorema = (cat1, cat2) => {
-  const result1 = cat1**2
-  const result2 = cat2**2
-  const pitagoras = result1 + result2
-
-  return console.log('c2:', pitagoras)
-}
-
-teorema(3, 4)
