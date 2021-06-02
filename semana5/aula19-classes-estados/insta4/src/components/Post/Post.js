f5d36d8c982c52e0b3027de764dc5eacbcb236d5
lucasgomesoficial <lucasgomesbr@gmail.com>
2021-06-02 16:26:59 -0300

    instabenu finalizado

---
+++ semana5/aula19-classes-estados/insta4/src/components/Post/Post.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+import { IconeComContador } from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import { SecaoComentario } from '../SecaoComentario/SecaoComentario'
+
+import { PostContainer, PostHeader, PostFooter, UserPhoto, PostPhoto } from './Styled'
+
+class Post extends React.Component {
+  state = {
+    curtido: false,
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0
+  }
+
+  onClickCurtida = () => {
+    this.setState({
+      curtido: !this.state.curtido
+    })
+    if (this.state.curtido) {
+      this.setState({
+        numeroCurtidas: 0
+      })
+    } else if (!this.state.curtido) {
+      this.setState({
+        numeroCurtidas: 1
+      })
+    }
+  }
+
+  onClickComentario = () => {
+    this.setState({
+      comentando: !this.state.comentando
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentando: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  render() {
+    let iconeCurtida
+
+    if (this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if (this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario} />
+    }
+
+    return <PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'} />
+        <p>{this.props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'} />
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
+  }
+}
+
+export default Post
