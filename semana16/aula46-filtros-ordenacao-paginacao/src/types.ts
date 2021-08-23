34342d12f7e9d116ffdb6028f2998912d771da08
lucasgomesoficial <lucasgomesbr@gmail.com>
2021-08-23 16:43:57 -0300

    Aula 46 Finalizado

---
+++ semana16/aula46-filtros-ordenacao-paginacao/src/types.ts
@@ -0,0 +1,22 @@
+export type recipe = {
+   id: string
+   title: string
+   description: string
+   userId: string
+   createdAt: number
+}
+
+export type user = {
+   id: string
+   name: string
+   email: string
+   password: string
+   recipes?: recipe[]
+}
+
+export type teachers = {
+   id: number
+   name: string
+   email: string
+   type: string
+}
